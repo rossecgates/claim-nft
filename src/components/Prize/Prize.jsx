929a446814d93255de9e82e966e75a54ab036003
Ivan <ikharchenko2008@gmail.com>
2021-09-07 16:50:39 +0300

    add mobile styles for prize block

--- src/components/Prize/Prize.jsx
+++ src/components/Prize/Prize.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
+import { ReactSVG } from 'react-svg';
 import PrizeList from './PrizeList';
+import background from '../../assets/images/prize-background.svg';
 
 const Prize = () => (
   <div className="prize">
-    <header>
-      <h2 className="prize__title">You can win</h2>
-    </header>
-    <PrizeList className="prize__list" />
+    <ReactSVG className="prize__background" src={background} />
+
+    <div>
+      <header>
+        <h2 className="prize__title">You can win</h2>
+      </header>
+      <PrizeList className="prize__list" />
+    </div>
   </div>
 );
 
