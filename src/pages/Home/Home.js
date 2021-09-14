89e4d236bb58dcd426c86fd8b1f95d43e48c911c
Ivan <ikharchenko2008@gmail.com>
2021-09-14 15:17:55 +0300

    update component

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,32 +1,40 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import Prize from '../../components/Prize';
 import SignForm from '../../components/SignForm';
 import Footer from '../../layouts/Footer';
 import Header from '../../layouts/Header';
 
 const Home = () => {
-  const isSignIn = true;
+  const history = useHistory();
+  const checkStatus = () => {
+    history.push('/login');
+  };
 
   return (
     <div className="home">
-      <Header />
+      <Header className="home__header" />
       <div className="home__data">
         <Footer />
         <main>
           <div className="home__title">
             <span className="home__title-earn">Earn rewards</span> by referring
-            the
-            <br />
-            best marketing agencies
+            the&nbsp;
+            <br /> best marketing agencies
           </div>
-          <Prize />
+          <Prize className="home__prize" />
           <p className="home__sign-up-text">Signup below and start referring</p>
           <SignForm />
           <p className="home__statistic">
             Already joined?&nbsp;
-            <span className="home__statistic-status">Check your status</span>
+            <button
+              type="button"
+              onClick={checkStatus}
+              className="home__statistic-status"
+            >
+              Check your status
+            </button>
           </p>
-          <SignForm isSignIn={isSignIn} />
         </main>
       </div>
     </div>
