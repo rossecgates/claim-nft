d9785a8563479f45d3dc55916d919308a93f3ddb
Ivan <ikharchenko2008@gmail.com>
2021-09-14 15:15:55 +0300

    add Data and Email contexts for component

--- src/layouts/Layout/Layout.jsx
+++ src/layouts/Layout/Layout.jsx
@@ -1,7 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
+import EmailContext from '../../contexts/EmailContext';
+import DataContext from '../../contexts/DataContext';
 
-const Layout = ({ children }) => <div className="layout">{children}</div>;
+const initData = {
+  current_priority: 0,
+  referral_link: 'https://near.org',
+  total_referrals: 0,
+  total_users: 0,
+};
+const Layout = ({ children }) => {
+  const [email, setEmail] = useState('');
+  const [data, setData] = useState(initData);
+
+  return (
+    <EmailContext.Provider value={{ email, setEmail }}>
+      <DataContext.Provider value={{ data, setData }}>
+        <div className="layout">{children}</div>
+      </DataContext.Provider>
+    </EmailContext.Provider>
+  );
+};
 
 Layout.propTypes = {
   children: PropTypes.element,
