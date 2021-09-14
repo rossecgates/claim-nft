682a54e4c13869162f3a67dac8b6e9dcec7508d9
Ivan <ikharchenko2008@gmail.com>
2021-09-14 15:16:42 +0300

    update for show text with condition

--- src/layouts/Header/Header.js
+++ src/layouts/Header/Header.js
@@ -1,18 +1,48 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { useLocation } from 'react-router';
 import Logo from '../../components/Logo';
 
-const Header = () => (
-  <div className="header">
-    <Logo />
+const Header = ({ className }) => {
+  const { pathname } = useLocation();
 
-    <div className="header__text">
-      Join the Crypto Marketing Conference&nbsp;&&nbsp;Contest
+  const getHeaderText = () => {
+    switch (pathname) {
+      case '/':
+        return (
+          <div className={`header__text ${className || ''} `}>
+            Join the Crypto Marketing Conference&nbsp;&&nbsp;Contest
+          </div>
+        );
+      case '/sign-in':
+        return (
+          <div className={`header__text ${className || ''}`}>
+            <span className="header__text-color">Congrats!</span> You are signed
+            up and ready to start referring.
+          </div>
+        );
+      case '/login':
+        return (
+          <div className={`header__text ${className || ''} `}>
+            <span className="header__text-color header__text-login">Login</span>{' '}
+            to continue
+          </div>
+        );
+      default:
+        return undefined;
+    }
+  };
+
+  return (
+    <div className="header">
+      <Logo />
+      {getHeaderText()}
     </div>
-    {/* <div className="header__text">
-      <span className="header__text-color">Congrats!</span> You are signed up
-      and ready to start referring.
-    </div> */}
-  </div>
-);
+  );
+};
+
+Header.propTypes = {
+  className: PropTypes.string,
+};
 
 export default Header;
