ae43929c666eee1b42f59013486a7841bfde3312
Ross Gates <gates.c.rosse@gmail.com>
2021-09-19 17:08:26 -0400

    updated the details

--- src/components/Prize/PrizeList.jsx
+++ src/components/Prize/PrizeList.jsx
@@ -4,6 +4,13 @@ import PropTypes from 'prop-types';
 const PrizeList = ({ className }) => (
   <div className={`prize-list ${className || ''}`}>
     <div className="prize-list__item">
+      <p className="prize-list__amount">10%</p>
+      <p className="prize-list__text">
+        of the prize money for referring a winning team.
+      </p>
+    </div>
+
+    <div className="prize-list__item">
       <p className="prize-list__amount">$10k</p>
       <p className="prize-list__text">
         for referring the most attendees in October
@@ -14,13 +21,6 @@ const PrizeList = ({ className }) => (
       <p className="prize-list__amount">$10k</p>
       <p className="prize-list__text">
         for referring the most attendees in November
-      </p>
-    </div>
-
-    <div className="prize-list__item">
-      <p className="prize-list__amount">10%</p>
-      <p className="prize-list__text">
-        of the prize money for referring a winning team.
       </p>
     </div>
   </div>
