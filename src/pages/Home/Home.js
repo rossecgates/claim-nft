dcec17434327ce09f938a3c192c02909bf61928e
Ross Gates <gates.c.rosse@gmail.com>
2021-10-06 10:16:45 -0400

    added break line

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -28,7 +28,7 @@ const Home = () => {
           <Prize className="home__prize" />
           <p className="home__sign-up-text">Signup below and start referring</p>
           <SignForm />
-          <p className="footer__text">☑ receive emails from GenC</p>
+          <p className="footer__text"><br/>☑ receive emails from GenC</p>
           <p className="home__statistic">
             Already joined?&nbsp;
             <button
